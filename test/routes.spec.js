0e2bf3be64ba900e7d0f979ca2ae93c75db368ec
Christie Lynam <christielynam@gmail.com>
2017-10-10 21:37:12 -0600

    Add test file and boilerplate to html file

---
+++ test/routes.spec.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+
+  it('should return the home page with text', (done) => {
+    chai.request(server)
+    .get('/')
+    .end((error, response) => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.res.text.includes('BYOB');
+      done();
+    });
+  });
