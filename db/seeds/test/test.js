f4ae2aa8c3d62f6205cd151cb20c6747953a50fd
Christie Lynam <christielynam@gmail.com>
2017-10-11 09:51:53 -0600

    Add JWT dependencies

--- db/seeds/test/test.js
+++ db/seeds/test/test.js
@@ -15,43 +15,44 @@ exports.seed = function(knex, Promise) {
           {
             id: 3,
             type: 'food'
-          }, '*')
+          }, 'id')
         .then(type => {
+          console.log(type);
           return knex('holidays').insert([
             {
               id: 1,
               name: 'National Relaxation Day',
               fullDate: 'August 15',
               month: 'August',
-              type_id: 1
+              type_id: type[0]
             },
             {
               id: 2,
               name: 'National Red Wine Day',
               fullDate: 'August 28',
               month: 'August',
-              type_id: 2
+              type_id: type[1]
             },
             {
               id: 3,
               name: 'National Taco Day',
               fullDate: 'October 4',
               month: 'October',
-              type_id: 3
+              type_id: type[2]
             },
             {
               id: 4,
               name: 'National Pasta Day',
               fullDate: 'October 17',
               month: 'October',
-              type_id: 3
+              type_id: type[2]
             },
             {
               id: 5,
               name: 'Halloween',
               fullDate: 'October 31',
               month: 'October',
-              type_id: 1
+              type_id: type[0]
             }
           ])
         })
