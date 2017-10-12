78c3ad13de15513b81fe2a8778953ede6b23de1d
Christie Lynam <christielynam@gmail.com>
2017-10-11 22:11:09 -0600

    Add tests for all GET endpoints

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -39,13 +39,239 @@ describe('API Routes', () => {
     database.migrate.latest()
       .then(() => done())
       .catch(error => console.log(error));
-  });
+  })
 
   beforeEach((done) => {
     database.seed.run()
       .then(() => done())
       .catch(error => console.log(error));
-  });
+  })
 
+  describe('GET /api/v1/types', () => {
 
-});
+    it('should retrieve all types', (done) => {
+      chai.request(server)
+      .get('/api/v1/types')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(3);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('type');
+        response.body[0].type.should.equal('activity');
+        response.body[1].should.have.property('id');
+        response.body[1].id.should.equal(2);
+        response.body[1].should.have.property('type');
+        response.body[1].type.should.equal('drinking');
+        response.body[2].should.have.property('id');
+        response.body[2].id.should.equal(3);
+        response.body[2].should.have.property('type');
+        response.body[2].type.should.equal('food');
+        done();
+      })
+    })
+
+    it('should return a 404 if the path is incorrect', (done) => {
+      chai.request(server)
+      .get('/api/v1/turing')
+      .end((error, response) => {
+        response.should.have.status(404);
+        done();
+      })
+    })
+
+  })
+
+  describe('GET /api/v1/holidays', () => {
+
+    it('should retrieve all holidays', (done) => {
+      chai.request(server)
+      .get('/api/v1/holidays')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(5);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('National Relaxation Day');
+        response.body[0].should.have.property('fullDate');
+        response.body[0].fullDate.should.equal('August 15');
+        response.body[0].should.have.property('month');
+        response.body[0].month.should.equal('August');
+        response.body[0].should.have.property('type_id');
+        response.body[0].type_id.should.equal(1);
+        response.body[1].should.have.property('id');
+        response.body[1].id.should.equal(2);
+        response.body[1].should.have.property('name');
+        response.body[1].name.should.equal('National Red Wine Day');
+        response.body[1].should.have.property('fullDate');
+        response.body[1].fullDate.should.equal('August 28');
+        response.body[1].should.have.property('month');
+        response.body[1].month.should.equal('August');
+        response.body[1].should.have.property('type_id');
+        response.body[1].type_id.should.equal(2);
+        response.body[2].should.have.property('id');
+        response.body[2].id.should.equal(3);
+        response.body[2].should.have.property('name');
+        response.body[2].name.should.equal('National Taco Day');
+        response.body[2].should.have.property('fullDate');
+        response.body[2].fullDate.should.equal('October 4');
+        response.body[2].should.have.property('month');
+        response.body[2].month.should.equal('October');
+        response.body[2].should.have.property('type_id');
+        response.body[2].type_id.should.equal(3);
+        done();
+      })
+    })
+
+    it('should return a 404 if the path is incorrect', (done) => {
+      chai.request(server)
+      .get('/api/v1/denver')
+      .end((error, response) => {
+        response.should.have.status(404);
+        done();
+      })
+    })
+
+    it('should filter the holidays by query param', (done) => {
+      chai.request(server)
+      .get('/api/v1/holidays?month=August')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(2);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('National Relaxation Day');
+        response.body[0].should.have.property('fullDate');
+        response.body[0].fullDate.should.equal('August 15');
+        response.body[0].should.have.property('month');
+        response.body[0].month.should.equal('August');
+        response.body[0].should.have.property('type_id');
+        response.body[0].type_id.should.equal(1);
+        response.body[1].should.have.property('id');
+        response.body[1].id.should.equal(2);
+        response.body[1].should.have.property('name');
+        response.body[1].name.should.equal('National Red Wine Day');
+        response.body[1].should.have.property('fullDate');
+        response.body[1].fullDate.should.equal('August 28');
+        response.body[1].should.have.property('month');
+        response.body[1].month.should.equal('August');
+        response.body[1].should.have.property('type_id');
+        response.body[1].type_id.should.equal(2);
+        done();
+      })
+    })
+
+    it('should retrieve a specific holiday', (done) => {
+      chai.request(server)
+      .get('/api/v1/holidays/2')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(2);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('National Red Wine Day');
+        response.body[0].should.have.property('fullDate');
+        response.body[0].fullDate.should.equal('August 28');
+        response.body[0].should.have.property('month');
+        response.body[0].month.should.equal('August');
+        response.body[0].should.have.property('type_id');
+        response.body[0].type_id.should.equal(2);
+        done();
+      })
+    })
+
+  })
+
+  describe('GET /api/v1/holidays/:month', () => {
+
+    it.skip('should retrieve all the holidays in a specific month', (done) => {
+      chai.request(server)
+      .get('/api/v1/holidays/October')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(3);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('National Taco Day');
+        response.body[0].should.have.property('fullDate');
+        response.body[0].fullDate.should.equal('October 4');
+        response.body[0].should.have.property('month');
+        response.body[0].month.should.equal('October');
+        response.body[0].should.have.property('type_id');
+        response.body[0].type_id.should.equal(3);
+        response.body[1].should.have.property('id');
+        response.body[1].id.should.equal(2);
+        response.body[1].should.have.property('name');
+        response.body[1].name.should.equal('National Pasta Day');
+        response.body[1].should.have.property('fullDate');
+        response.body[1].fullDate.should.equal('October 17');
+        response.body[1].should.have.property('month');
+        response.body[1].month.should.equal('October');
+        response.body[1].should.have.property('type_id');
+        response.body[1].type_id.should.equal(2);
+        response.body[2].should.have.property('id');
+        response.body[2].id.should.equal(3);
+        response.body[2].should.have.property('name');
+        response.body[2].name.should.equal('Halloween');
+        response.body[2].should.have.property('fullDate');
+        response.body[2].fullDate.should.equal('October 31');
+        response.body[2].should.have.property('month');
+        response.body[2].month.should.equal('October');
+        response.body[2].should.have.property('type_id');
+        response.body[2].type_id.should.equal(1);
+        done();
+      })
+    })
+
+  })
+
+  describe('GET /api/v1/types/:id/holidays', () => {
+
+    it('should get all the holidays of a specific type', (done) => {
+      chai.request(server)
+      .get('/api/v1/types/1/holidays')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(2);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('National Relaxation Day');
+        response.body[0].should.have.property('fullDate');
+        response.body[0].fullDate.should.equal('August 15');
+        response.body[0].should.have.property('month');
+        response.body[0].month.should.equal('August');
+        response.body[0].should.have.property('type_id');
+        response.body[0].type_id.should.equal(1);
+        response.body[1].should.have.property('id');
+        response.body[1].id.should.equal(5);
+        response.body[1].should.have.property('name');
+        response.body[1].name.should.equal('Halloween');
+        response.body[1].should.have.property('fullDate');
+        response.body[1].fullDate.should.equal('October 31');
+        response.body[1].should.have.property('month');
+        response.body[1].month.should.equal('October');
+        response.body[1].should.have.property('type_id');
+        response.body[1].type_id.should.equal(1);
+        done();
+      })
+    })
+  })
+
+})
